c4850642a18328a1647006b46bb68c448cf5cc93
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-03-27 18:58:56 +0100

    feat: connect frontend with backend in dev and prod mode

--- backend/src/create-app.ts
+++ backend/src/create-app.ts
@@ -1,13 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 export const createApp = async () => {
   const app = express();
 
+  app.use(
+    cors({
+      origin:
+        process.env.NODE_ENV === "production"
+          ? process.env.FRONTEND_URL
+          : "http://localhost:3000",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
   app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.json({
+      message:
+        "If you are seeing this, the backend is connected to the frontend",
+    });
   });
   console.log("App created");
   return app;
--- frontend/components/home/home.tsx
+++ frontend/components/home/home.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export function Home() {
-  return <div>Test</div>;
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL || "");
+      const data = await res.json();
+      console.log(data);
+      setMessage(data.message);
+    };
+    fetchMessage();
+  }, []);
+  return <div>{message}</div>;
 }
